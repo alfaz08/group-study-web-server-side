1f8eebc59c8772a412ba750d2c0e8ab31a271a00
Alfaz Hossain <alfaz.cse@gmail.com>
2023-11-06 09:39:03 +0600

    delete data function is added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port,()=>{
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fpdogwm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,12 +36,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+   
+    //database collection
+    const assignmentCollection = client.db("groupStudyDB").collection("allassignment")
+
+
+
+
+    //post all assignment to the server
+
+    app.post('/allassignment',async(req,res)=>{
+      const newAssignment = req.body;
+      const result = await assignmentCollection.insertOne(newAssignment)
+      res.send(result)
+    })
+
+
+    //get all data
+
+    app.get('/allassignment',async(req,res)=>{
+      const cursor = assignmentCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+     //delete data
+     app.delete('/allassignment/:id',async(req,res)=>{
+      const id=req.params.id;
+      console.log('please delete this',id);
+      const query =  {_id: new ObjectId(id)}
+      const result = await assignmentCollection.deleteOne(query)
+      res.send(result)
+     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
